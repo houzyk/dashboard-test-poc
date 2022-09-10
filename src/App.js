93829ec29ef163dc49a21ff7a9b4cb068b88128b
Houzair Koussa <houzairmk@icloud.com>
2022-09-10 19:51:25 +0400

    [dev] ADD all dev fetch call

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+
+  const [developers, setDevelopers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api-test-poc-1.herokuapp.com/devs')
+    .then(response => response.json())
+    .then(data => setDevelopers(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Developers</h1>
+      { !!developers && developers.map(({
+        user,
+        name,
+        html_url,
+        avatar_url,
+        numTotalTests,
+        numPassedTests,
+        numFailedTests
+      }, index) => (
+        <>
+        </>
+      ))}
+    </>
   );
 }
 
